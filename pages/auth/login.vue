ff5eb61f655c6aaa59cdb49c711ff01c60ddbb61
iLuiizUHD <luizgomes749@gmail.com>
2021-02-13 19:20:56 -0300

    Too many login buttons

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -3,12 +3,28 @@
     <section class="login-screen w-2/4 h-56">
       <div class="login-card">
         <h2 class="login-card-title">Login with your account!</h2>
+        <p align="justify">
+          Please, to proceed to your account use your, Google, Twitter, Facebook
+          or GitHub accounts!
+        </p>
 
         <div class="login-form-fields pt-10">
-          <form action="" method="post" @submit.prevent>
-            <label for="email"> Email </label>
-            <input class="input-custom w-full" type="email" name="email" />
-          </form>
+          <button class="login-with-google">
+            <img src="/utils/Google__G__Logo.svg" alt="Google logo" />
+            <span>Sign-in with Google</span>
+          </button>
+          <button class="login-with-twitter">
+            <img src="/utils/twitter-bird-logo-white.svg" alt="Twitter logo" />
+            <span>Sign-in with Twitter</span>
+          </button>
+          <button class="login-with-facebook">
+            <img src="/utils/f_logo_RGB-White_250.png" alt="Facebook logo" />
+            <span>Sign-in with Facebook</span>
+          </button>
+          <button class="login-with-github">
+            <img src="/utils/GitHub-Mark-Light-64px.png" alt="Github logo" />
+            <span>Sign-in with GitHub</span>
+          </button>
         </div>
       </div>
     </section>
@@ -41,6 +57,106 @@ export default Vue.extend({});
 
     .login-card-title {
       @apply text-3xl;
+    }
+
+    .login-form-fields {
+      .login-with {
+        padding: 5px;
+        margin: auto;
+        width: 250px;
+
+        font-family: "Roboto", sans-serif;
+        font-weight: 500;
+
+        @apply flex;
+        @apply align-middle;
+        @apply rounded-sm;
+        @apply shadow-md;
+
+        background: #f9fafb;
+        @apply border;
+        @apply border-gray-200;
+
+        height: 40px;
+
+        img {
+          width: 18px;
+          height: 18px;
+          margin-right: 24px;
+          margin-left: 8px;
+
+          margin-top: auto;
+          margin-bottom: auto;
+        }
+
+        span {
+          margin-right: 8px;
+          margin-top: auto;
+          margin-bottom: auto;
+        }
+
+        &:hover {
+          @apply bg-gray-200;
+        }
+
+        margin-bottom: 15px;
+      }
+
+      button.login-with-google {
+        @extend .login-with;
+
+        margin-bottom: 15px;
+      }
+
+      button.login-with-github {
+        @extend .login-with;
+
+        background: #2c2c2c;
+        @apply border;
+        @apply border-gray-800;
+
+        span {
+          color: white;
+        }
+
+        &:hover {
+          background: #151515;
+        }
+
+        margin-bottom: 15px;
+      }
+
+      button.login-with-twitter {
+        @extend .login-with;
+
+        background: #1d9bf0;
+
+        span {
+          color: white;
+        }
+
+        &:hover {
+          background: #1189d8;
+        }
+
+        margin-bottom: 15px;
+      }
+
+      button.login-with-facebook {
+        @extend .login-with;
+
+        background: #1778f2;
+
+        span {
+          color: white;
+        }
+
+        &:hover {
+          background: #1469d1;
+        }
+
+        margin-bottom: 15px;
+      }
     }
   }
 }
